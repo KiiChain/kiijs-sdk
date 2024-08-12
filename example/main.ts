72a2376dc97cffe8f87670edc4ae44cb9a2b7e2d
RedBird96 <gt20181122@gmail.com>
2024-08-12 14:49:03 -0500

    add evm stuff

--- example/main.ts
+++ example/main.ts
@@ -1,15 +1,15 @@
 import {KiiStargateQueryClient} from "../src";
 
 async function main() {
-  const rpcEndpoint = "https://a.testnet.kiivalidator.com:26658/";
+  // const rpcEndpoint = "https://a.testnet.kiivalidator.com:26658/";
+  const rpcEndpoint = "https://a.sentry.testnet.kiivalidator.com:8645/";
 
   const client = await KiiStargateQueryClient.connect(
     rpcEndpoint
   );
 
-  const address = "kii1s0jekzmfy3ejmf75lh0xfc2zl3958lfk8gqtws";
-  const balance = await client.getAllBalances(address);
-  console.log('balance', balance);
+  const bal = await client.getBalance("0x84c86D737C4257FF5B8F42788c17D8C0b99f7A08");
+  console.log("bal", bal);
 }
 
 main().catch((err) => console.error(err))
