8cc18e5c68cfbd1dc840452d6abc98107f1bfad3
Thales Zirbel <thales.zirbel@brickabode.com>
2025-04-07 15:34:43 -0300

    lint: get correct default naming

--- packages/proto/tests/connect.test.ts
+++ packages/proto/tests/connect.test.ts
@@ -4,17 +4,17 @@ import { SigningStargateClient} from '@cosmjs/stargate';
 
 jest.setTimeout(30_000); // Total test timeout
 
-describe('Sign connection Test', () => {
+describe('sign connection Test', () => {
   let client: SigningStargateClient; 
   let wallet: DirectSecp256k1HdWallet; 
 
-  it('Setup client and wallet failed', async () => {
+  it('setup client and wallet failed', async () => {
     [client, wallet] = await withRetry(() => setupTestClient());
     expect(client).toBeDefined();
     expect(wallet).toBeDefined();
   });
   
-  it('Account should have balance', async () => {
+  it('account should have balance', async () => {
     const [account] = await wallet.getAccounts();
     const coin = await client.getBalance(account.address, 'ukii')
     expect(Number(coin.amount)).toBeGreaterThan(0);
