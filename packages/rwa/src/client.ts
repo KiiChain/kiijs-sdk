d13b1cabff3e3365255d73ce620db557b26d999b
Thales Zirbel <thales.zirbel@brickabode.com>
2025-07-23 11:17:05 -0300

    chore: clean client code

--- packages/rwa/src/client.ts
+++ packages/rwa/src/client.ts
@@ -12,15 +12,6 @@ export interface AccountInfoResponse {
   sequence: number;
 }
 
-export interface ExecuteResponse {
-  tx_hash: string;
-  data: Uint8Array;
-  gas_used: number;
-  gas_wanted: number;
-  events: readonly any[]; // Changed to readonly
-  height: number;
-}
-
 const myRegistry = new Registry(defaultRegistryTypes);
 myRegistry.register("/cosmwasm.wasm.v1.MsgExecuteContract", MsgExecuteContract);
 
@@ -45,48 +36,16 @@ export class RwaClient {
     this.gasPrice = gasPrice;
   }
 
-  public static async new(
-    rpcUrl: string,
-    chainId: string,
-    denom: string,
-    gasPrice: string,
-  ): Promise<RwaClient> {
-    // For now, we'll create a dummy signer for the connectWithSigner method.
-    // In a real application, this would come from user input or a wallet.
-    const mnemonic = "guilt elder dance satisfy pencil chuckle bronze behave rate film tumble flash";
-    const wallet = await DirectSecp256k1HdWallet.fromMnemonic(mnemonic, { prefix: "cosmos" });
-    const [firstAccount] = await wallet.getAccounts();
-
-    const rpcClient = await SigningStargateClient.connectWithSigner(
-      rpcUrl,
-      wallet,
-      { registry: myRegistry },
-    );
-
-    const queryClient = await CosmWasmClient.connect(rpcUrl);
-
-    return new RwaClient(
-      rpcClient,
-      queryClient,
-      chainId,
-      denom,
-      gasPrice,
-    );
-  }
-
   /**
    * Creates a new RwaClient with a provided signer.
    * @param rpcUrl - The RPC URL of the blockchain
    * @param chainId - The chain ID
-   * @param tokenAddress - The token contract address
-   * @param identityAddress - The identity contract address
-   * @param complianceAddress - The compliance contract address
    * @param denom - The denomination for fees
    * @param gasPrice - The gas price
    * @param signer - The wallet signer
    * @returns Promise<RwaClient> - A new RwaClient instance
    */
-  public static async newWithSigner(
+  public static async new(
     rpcUrl: string,
     chainId: string,
     denom: string,
@@ -108,25 +67,6 @@ export class RwaClient {
       denom,
       gasPrice,
     );
-  }
-
-  /**
-   * Gets account information for an address.
-   */
-  public async getAccountInfo(address: string): Promise<AccountInfoResponse> {
-    try {
-      const account = await this.rpcClient.getAccount(address);
-      if (account) {
-        return {
-          account_number: account.accountNumber,
-          sequence: account.sequence,
-        };
-      }
-      return { account_number: 0, sequence: 0 };
-    } catch (error) {
-      console.error('Error getting account info:', error);
-      return { account_number: 0, sequence: 0 };
-    }
   }
 
   /**
