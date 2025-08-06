30715d9142f604ed2a3f08d04a5d66a5eb2aa122
senjaparades <dwi.adigunawan.spt@gmail.com>
2025-08-06 12:28:24 +0700

    style: format get-balance-example.js using Prettier

--- examples/examples/get-balance-example.js
+++ examples/examples/get-balance-example.js
@@ -9,8 +9,7 @@ const RPC_URL =
   process.env.RPC_URL ||
   'https://json-rpc.uno.sentry.testnet.v3.kiivalidator.com/';
 const ADDRESS =
-  process.env.ADDRESS ||
-  '0xAB1F600358Acd5A163d9E2C64a8840a267F983F9'; // ✅ Valid checksum
+  process.env.ADDRESS || '0xAB1F600358Acd5A163d9E2C64a8840a267F983F9'; // ✅ Valid checksum
 
 const provider = new ethers.JsonRpcProvider(RPC_URL);
 
